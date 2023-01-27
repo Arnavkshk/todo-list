1411cff46bde1b829b5b7dce20a0ae0be7979649
ARNAV KAUSHIK <arnav22112003@gmail.com>
2023-01-28 02:19:00 +0530

    made todo list using react

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import List from './components/List';
+function App() {
+    return(
+        <div className='todoapp'>
+            <List/>
+        </div>
+        );
+    }
+export default App;
